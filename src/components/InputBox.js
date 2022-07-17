abb9029e0299eb82e406ef068e826cda7aaf171a
hwchak1119 <hwchak1119@gmail.com>
2022-07-17 19:46:28 +0800

    Save in Redux state

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,11 +1,22 @@
 import React, { useState } from "react";
 import { Flex, Input, Button } from "@chakra-ui/react";
 import { AddIcon } from "@chakra-ui/icons";
+import { useDispatch } from "react-redux";
+import { saveTodo } from "../features/todoSlice";
 
 function InputBox() {
   const [input, setInput] = useState("");
+  const dispatch = useDispatch();
 
-  const addTodo = () => {};
+  const addTodo = () => {
+    dispatch(
+      saveTodo({
+        item: input,
+        done: false,
+        id: Date.now(),
+      })
+    );
+  };
 
   return (
     <Flex>
@@ -15,7 +26,7 @@ function InputBox() {
         onChange={(e) => setInput(e.target.value)}
         mr='12px'
       />
-      <Button rightIcon={<AddIcon />} colorScheme='blue'>
+      <Button rightIcon={<AddIcon />} colorScheme='blue' onClick={addTodo}>
         Add
       </Button>
     </Flex>
